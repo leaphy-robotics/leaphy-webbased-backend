d91bc55a030c6a875b10e99150494080b274b7ab
sverben <59171289+sverben@users.noreply.github.com>
2025-07-23 13:29:01 +0200

    chore: black

--- deps/utils.py
+++ deps/utils.py
@@ -1,10 +1,18 @@
-def bin2header(data, var_name='var'):
+def bin2header(data, var_name="var"):
     out = []
-    out.append('unsigned char {var_name}[] = {{'.format(var_name=var_name))
-    l = [ data[i:i+12] for i in range(0, len(data), 12) ]
+    out.append("unsigned char {var_name}[] = {{".format(var_name=var_name))
+    l = [data[i : i + 12] for i in range(0, len(data), 12)]
     for i, x in enumerate(l):
-        line = ', '.join([ '0x{val:02x}'.format(val=c) for c in x ])
-        out.append('  {line}{end_comma}'.format(line=line, end_comma=',' if i<len(l)-1 else ''))
-    out.append('};')
-    out.append('unsigned int {var_name}_len = {data_len};'.format(var_name=var_name, data_len=len(data)))
-    return '\n'.join(out)
+        line = ", ".join(["0x{val:02x}".format(val=c) for c in x])
+        out.append(
+            "  {line}{end_comma}".format(
+                line=line, end_comma="," if i < len(l) - 1 else ""
+            )
+        )
+    out.append("};")
+    out.append(
+        "unsigned int {var_name}_len = {data_len};".format(
+            var_name=var_name, data_len=len(data)
+        )
+    )
+    return "\n".join(out)
