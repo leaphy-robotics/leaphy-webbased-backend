66e04df409e62cd21b22d06e71c0cb931dee5c62
sverben <59171289+sverben@users.noreply.github.com>
2026-01-11 12:02:18 +0100

    fix: don't require mail server

--- deps/mail.py
+++ deps/mail.py
@@ -3,22 +3,30 @@
 from fastapi_mail import ConnectionConfig, MessageSchema, FastMail
 
 from conf import settings
+from deps.logs import logger
 
-mail_config = ConnectionConfig(
-    MAIL_SERVER=settings.mail_server,
-    MAIL_PORT=settings.mail_port,
-    MAIL_USERNAME=settings.mail_username,
-    MAIL_PASSWORD=settings.mail_password,
-    MAIL_FROM_NAME=settings.mail_from,
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
-    MAIL_FROM=settings.mail_from,
-    USE_CREDENTIALS=True,
-)
+MAIL_CONFIG = None
+
+if settings.mail_server:
+    MAIL_CONFIG = ConnectionConfig(
+        MAIL_SERVER=settings.mail_server,
+        MAIL_PORT=settings.mail_port,
+        MAIL_USERNAME=settings.mail_username,
+        MAIL_PASSWORD=settings.mail_password,
+        MAIL_FROM_NAME=settings.mail_from,
+        MAIL_STARTTLS=True,
+        MAIL_SSL_TLS=False,
+        MAIL_FROM=settings.mail_from,
+        USE_CREDENTIALS=True,
+    )
 
 
 async def send_email(subject: str, body: str, attachments: list = None):
     """Send an email"""
+    if not MAIL_CONFIG:
+        logger.warning("Mail server not configured, skipping email")
+        return
+
     message = MessageSchema(
         subject=subject,
         body=body,
@@ -27,5 +35,5 @@ async def send_email(subject: str, body: str, attachments: list = None):
         attachments=attachments,
     )
 
-    fm = FastMail(mail_config)
+    fm = FastMail(MAIL_CONFIG)
     await fm.send_message(message)
